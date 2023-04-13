b0ea28b9b273c77f97ccb1fd3963330022060a54
AdrielModollo <adrielmodollo@outlook.com>
2023-04-13 17:04:32 -0300

    feat: receveid date by id

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,4 +1,33 @@
+import { useParams } from "react-router"
+import { useState, useEffect } from "react"
+import { APIKey } from '../../config/key'
+
 function Details() {
+
+    const { id } = useParams()
+
+    const { movie, setMovies } = useState({})
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${APIKey}&language=en-US`)
+            .then(response => response.json())
+            .then(data => {
+
+                const { title, overview, backdrop_path, release_date } = data
+
+                const movie = {
+                    id,
+                    title,
+                    sinopse: overview,
+                    image: backdrop_path,
+                    releaseDate: release_date
+                }
+
+                console.log(movie)
+                setMovies(movie)
+            })
+    }, [id])
+
     return (
         <h1> Página de detalhes </h1>
     )
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,13 @@
 import { Container, MovieList, Movie } from "./styles"
 import { APIKey } from '../../config/key'
 import { useState, useEffect } from 'react'
-
+import { Link } from "react-router-dom"
 
 function Home() {
 
     const [movies, setMovies] = useState([])
 
     useEffect(() => {
-        // consumir a api...
         fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${APIKey}&language=en-US&page=1`)
             .then(response => response.json())
             .then(data => {
@@ -24,10 +23,11 @@ function Home() {
                 {movies.map(movie => {
                     return (
                         <Movie key={movie.id}>
-                            <a href="https://google.com">
-                                <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-                            </a>
+
+                            <Link to={`/details/${movie.id}`}><img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} /></Link>
+
                             <span>{movie.title}</span>
+
                         </Movie>
                     )
                 })}
