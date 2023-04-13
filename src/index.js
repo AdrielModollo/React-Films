f45d2ac65d5f5bf153134ba527c033d18a59cd36
AdrielModollo <adrielmodollo@outlook.com>
2023-04-13 16:41:04 -0300

    feat: prepare route details

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './global.css';
 import Home from './pages/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Details from './pages/Details';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />}></Route>
+      <Route path='/details/:id' element={<Details />}></Route>
     </Routes>
-    <Home />
   </BrowserRouter>
 );
---
+++ src/pages/Details/index.js
@@ -0,0 +1,7 @@
+function Details() {
+    return (
+        <h1> Página de detalhes </h1>
+    )
+}
+
+export default Details
