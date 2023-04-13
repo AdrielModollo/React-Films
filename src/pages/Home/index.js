8de0002fadd65c93126cb3d4bc477883e928c2c4
AdrielModollo <adrielmodollo@outlook.com>
2023-04-13 09:28:23 -0300

    feat: implement li

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,24 @@
 function Home() {
     return (
-        <h1> Hello, World!</h1>
+        <div>
+            <h1> Movies!</h1>
+            <ul>
+                <li>
+                    <img src="https://sm.ign.com/ign_br/movie/s/super-mari/super-mario-bros-o-filme_hnx9.jpg" alt="Mario" />
+                    <span>Mario</span>
+                </li>
+
+                <li>
+                    <img src="https://sm.ign.com/ign_br/movie/s/super-mari/super-mario-bros-o-filme_hnx9.jpg" alt="Mario" />
+                    <span>Mario</span>
+                </li>
+
+                <li>
+                    <img src="https://sm.ign.com/ign_br/movie/s/super-mari/super-mario-bros-o-filme_hnx9.jpg" alt="Mario" />
+                    <span>Mario</span>
+                </li>
+            </ul>
+        </div>
     )
 }
 
