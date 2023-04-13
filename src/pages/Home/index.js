a3ab1bf36e10c99ea1dbeade58bbbd65d9edc9dd
AdrielModollo <adrielmodollo@outlook.com>
2023-04-13 12:25:08 -0300

    feat: static to dynamic

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,30 +1,38 @@
 import { Container, MovieList, Movie } from "./styles"
 
 function Home() {
+
+    const movies = [
+        {
+            title: 'Mario Brothers',
+            image_url: "https://sm.ign.com/ign_br/movie/s/super-mari/super-mario-bros-o-filme_hnx9.jpg"
+        },
+        {
+            title: 'Avenger',
+            image_url: "https://sm.ign.com/ign_br/movie/s/super-mari/super-mario-bros-o-filme_hnx9.jpg"
+        },
+        {
+            title: 'Star wars',
+            image_url: "https://sm.ign.com/ign_br/movie/s/super-mari/super-mario-bros-o-filme_hnx9.jpg"
+        }
+    ]
+
     return (
         <Container>
             <h1> Movies!</h1>
             <MovieList>
-                <Movie>
-                    <a href="https://google.com">
-                        <img src="https://sm.ign.com/ign_br/movie/s/super-mari/super-mario-bros-o-filme_hnx9.jpg" alt="Mario" />
-                    </a>
-                    <span>Mario</span>
-                </Movie>
 
-                <Movie>
-                    <a href="https://google.com">
-                        <img src="https://sm.ign.com/ign_br/movie/s/super-mari/super-mario-bros-o-filme_hnx9.jpg" alt="Mario" />
-                    </a>
-                    <span>Mario</span>
-                </Movie>
+                {movies.map(movie => {
+                    return (
+                        <Movie>
+                            <a href="https://google.com">
+                                <img src={movie.image_url} alt={movie.title} />
+                            </a>
+                            <span>{movie.title}</span>
+                        </Movie>
+                    )
+                })}
 
-                <Movie>
-                    <a href="https://google.com">
-                        <img src="https://sm.ign.com/ign_br/movie/s/super-mari/super-mario-bros-o-filme_hnx9.jpg" alt="Mario" />
-                    </a>
-                    <span>Mario</span>
-                </Movie>
             </MovieList>
         </Container>
     )
